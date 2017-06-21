86b94a0ff9d278ccf6f8dfe4efa68650e681bfe8
te <longtranf@gmail.com>
2017-06-21 17:48:50 +0700

    Modified solution for hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -6,28 +6,16 @@ import (
 
 const testVersion = 6
 
+// Distance : Function get difference
 func Distance(a, b string) (int, error) {
-	var result, state int
-	state = 1
+	var result int
 
 	if len(a) != len(b) {
-		return -1, errors.New("Two string doesn't have the same len!")
+		return -1, errors.New("two string doesn't have the same len")
 	}
 
-	for i := 0; i < len(a); i++ {
-		switch state {
-		case 1:
-			switch {
-			case i == len(a):
-				return result + len(b) - len(a), nil
-			case i == len(b):
-				state = 2
-				result++
-			case b[i] != a[i]:
-				result++
-			default:
-			}
-		case 2:
+	for i, value := range a {
+		if byte(value) != b[i] {
 			result++
 		}
 	}
