b601523e69e92435640b2b08a178dbb59cd67479
te <longtranf@gmail.com>
2017-06-23 17:57:14 +0700

    Update house solution

--- go/house/house.go
+++ go/house/house.go
@@ -2,10 +2,35 @@ package house
 
 const testVersion = 1
 
+var strReplace = map[string][2]string{
+	"is":      {"malt", "lay"},
+	"lay":     {"rat", "ate"},
+	"ate":     {"cat", "killed"},
+	"killed":  {"dog", "worried"},
+	"worried": {"cow with the crumpled horn", "tossed"},
+	"tossed":  {"maiden all forlorn", "milked"},
+	"milked":  {"man all tattered and torn", "kissed"},
+	"kissed":  {"priest all shaven and shorn", "married"},
+	"married": {"rooster that crowed in the morn", "woke"},
+	"woke":    {"the farmer sowing his corn", "kept"},
+	"kept":    {"the horse and the hound and the horn", "belonged"},
+}
+
+var pre = "This"
+var after = "that"
+
 // Song : All house song
 func Song() string {
 
-	return ""
+	verb := "is"
+	pharse := "house that Jack built"
+	song := pre + " " + verb + " " + pharse
+
+	for i := 0; i < 12; i++ {
+		// later
+
+	}
+	return song
 }
 
 // Verse : Verse the song
